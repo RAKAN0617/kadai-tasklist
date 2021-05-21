9c45d77b8d0fb59b72cedd5c8e80b901796f71ed
michihisa.endou <aws.techacademy@gmail.com>
2021-05-21 17:29:41 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
@@ -49,7 +50,7 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
