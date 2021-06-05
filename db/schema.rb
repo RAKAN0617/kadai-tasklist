bb5243098c459310c0ca02429ba13550dc62edab
michihisa.endou <aws.techacademy@gmail.com>
2021-06-05 11:09:10 +0000

    colam reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_124240) do
+ActiveRecord::Schema.define(version: 2021_06_05_110614) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
